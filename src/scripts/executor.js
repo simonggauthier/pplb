dbba16932f5d103931c2233a7e55ca95ef2c548d
Simon Gregoire-Gauthier <simon.ggauthier@gmail.com>
2025-02-27 23:04:27 -0500

    Calibrations

--- src/scripts/executor.js
+++ src/scripts/executor.js
@@ -16,13 +16,13 @@ export default class Executor {
 
         while (total > 0) {
             await this.controller.moveTo(4, 1);
+            await this.controller.depositToBank('small_health_potion', page / 3);
+
+            await this.controller.moveTo(4, 1);
             await this.controller.withdrawFromBank('sunflower', page);
 
             await this.controller.moveTo(2, 3);
             await this.controller.craft('small_health_potion', page / 3);
-
-            await this.controller.moveTo(4, 1);
-            await this.controller.depositToBank('small_health_potion', page / 3);
 
             total -= page;
         }
